5d055dd43eab0913f94ffafb1b6c5dae912438d7
awsr <43862868+awsr@users.noreply.github.com>
2025-10-06 00:13:22 -0700

    Minor code cleanup
    - Add comment to explain why the extra logic steps are here.
    - Import "version" as "pkg_ver" to make sure nobody mixes it up with the "__version__" a few lines later.

--- modules/onnx_impl/__init__.py
+++ modules/onnx_impl/__init__.py
@@ -241,8 +241,9 @@ def initialize_onnx():
         diffusers.ORTStableDiffusionXLPipeline = diffusers.OnnxStableDiffusionXLPipeline # Huggingface model compatibility
         diffusers.ORTStableDiffusionXLImg2ImgPipeline = diffusers.OnnxStableDiffusionXLImg2ImgPipeline
 
-        from importlib.metadata import version
-        opt_ver = version('optimum').split('.')
+        # ORTPipelinePart was only used from optimum versions 1.23.0 through 1.25.3
+        from importlib.metadata import version as pkg_ver
+        opt_ver = pkg_ver('optimum').split('.')
         if opt_ver[0] == "1" and 23 <= int(opt_ver[1]) < 26:
             optimum.onnxruntime.modeling_diffusion.ORTPipelinePart.to = ORTDiffusionModelPart_to # pylint: disable=protected-access
 
